83a67a6bd473ee44f15cb9c7b4659d9cfe5d8649
Shangxianjiang <shangxianjiang@gmail.com>
2024-06-19 22:12:51 +0800

    python createsuperuser

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    """用户的学习主题"""
+    text = models.CharField(max_length=200)
+    date_add = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        return super().__str__()
